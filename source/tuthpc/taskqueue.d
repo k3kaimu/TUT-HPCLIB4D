a956ad4a4da8191974274e0e355b7321d5a26395
Kazuki Komatsu <komatsu@comm.ee.tut.ac.jp>
2017-12-15 14:16:35 +0900

    プロセス数の制限をクラスタでのみ適用するように修正

--- source/tuthpc/taskqueue.d
+++ source/tuthpc/taskqueue.d
@@ -356,7 +356,8 @@ Pid spawnTask(JobEnvironment jenv, size_t taskIndex, string logdir, string file,
 {
     import std.path;
 
-    enforce(numOfProcessOfUser() <= jenv.totalProcessNum, "Many processes are spawned.");
+    if(nowRunningOnClusterDevelopmentHost() || nowRunningOnClusterComputingNode())
+        enforce(numOfProcessOfUser() <= jenv.totalProcessNum, "Many processes are spawned.");
 
     string outname = buildPath(logdir, format("stdout_%s.log", taskIndex));
     string errname = buildPath(logdir, format("stderr_%s.log", taskIndex));
