1b1d82fb76990defd77f75466746ad035c8db8d9
Kazuki Komatsu <komatsu@comm.ee.tut.ac.jp>
2019-07-31 13:10:41 +0900

    add more example in exmaple/variable_test

--- examples/variable_test/source/app.d
+++ examples/variable_test/source/app.d
@@ -1,8 +1,11 @@
-import tuthpc.variable;
-import tuthpc.taskqueue;
-import std.stdio;
+import std.algorithm;
+import std.conv;
 import std.format;
+import std.range;
+import std.stdio;
 import std.typecons;
+import tuthpc.taskqueue;
+import tuthpc.variable;
 
 
 /**
@@ -14,26 +17,32 @@ import std.typecons;
 */
 void main()
 {
-    auto env = defaultJobEnvironment();
+    {
+        auto env = defaultJobEnvironment();
 
-    auto taskList = new MultiTaskList!int();
+        auto taskList = new MultiTaskList!int();
 
-    foreach(i; 0 .. 10) {
-        taskList.append((size_t i) {
-            return cast(int) i^^2;
-        }, i);
+        foreach(i; 0 .. 10) {
+            taskList.append((size_t i) {
+                return cast(int) i^^2;
+            }, i);
+        }
+
+        // ジョブ投入 & 実行
+        auto res = taskList.run(env);
+
+        // ジョブの実行結果の表示
+        if(env.isShowMode) {
+            foreach(i, e; res.retvals){
+                if(e.isNull)
+                    writefln!"%s: null"(i);
+                else
+                    writefln!"%s: %s"(i, e.get);
+            }
+        }
     }
 
-    // ジョブ投入 & 実行
-    auto res = taskList.run(env);
-
-    // ジョブの実行結果の表示
-    if(env.isShowMode) {
-        foreach(i, e; res.retvals){
-            if(e.isNull)
-                writefln!"%s: null"(i);
-            else
-                writefln!"%s: %s"(i, e.get);
-        }
+    {
+        iota(10).toTasks!(a => a^^2).run().retvals.writeln();
     }
 }
